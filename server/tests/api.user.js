e131620d9bee302bab23911c65949d272a7cc4e9
Ben Bledsoe <bledsoedesigns@gmail.com>
2018-01-31 16:06:31 -0600

    Adding tests for /api/users/:id routes

--- server/tests/api.user.js
+++ server/tests/api.user.js
@@ -7,6 +7,8 @@ const { ADMIN_SECRET_KEY } = require('../../config')
 chai.use(chaiHttp)
 
 describe('/api/users endpoints', () => {
+		
+    // test general listing of all users
     describe('/users', () => {
         it('should not retrieve users if authorization header does not exist', (done) => {
 					chai.request('http://localhost:4000')
@@ -36,4 +38,67 @@ describe('/api/users endpoints', () => {
 					})
 				})
     })
+		
+    // test general listing of one user
+		describe('GET /users/:id', () => {
+				it('should not retrieve user if invalid id is passed in', (done) => {
+					chai.request('http://localhost:4000')
+					.get('/api/users/someInvalidId')
+					.end((err, res) => {
+						expect(res.body.error).to.equal(true)
+						done()
+					})
+				})
+				it('should not retrieve user if no user exists with specified id', (done) => {
+					chai.request('http://localhost:4000')
+					.get('/api/users/9999999999')
+					.end((err, res) => {
+						expect(res.body.error).to.equal(true)
+						done()
+					})
+				})
+				it('should retrieve user if valid id is specified', (done) => {
+					chai.request('http://localhost:4000')
+					.get('/api/users/1')
+					.end((err, res) => {
+						expect(res.body.error).to.equal(false)
+						expect(res.body.data[0].id).to.equal(1)
+						done()
+					})
+				})
+		})
+		
+    // test ability to edit a user
+		describe('POST /users/:id', () => {
+				it('should not update user if invalid id is passed in', (done) => {
+					chai.request('http://localhost:4000')
+					.post('/api/users/someInvalidId')
+					.end((err, res) => {
+						expect(res.body.error).to.equal(true)
+						done()
+					})
+				})
+				it('should not update user if no user exists with specified id', (done) => {
+					chai.request('http://localhost:4000')
+					.post('/api/users/88888888888')
+					.end((err, res) => {
+						expect(res.body.error).to.equal(true)
+						done()
+					})
+				})
+				it('should update user if valid id is specified', (done) => {
+					const firstName = 'testing_from_tests'+Date.now().toString()
+					
+					chai.request('http://localhost:4000')
+					.post('/api/users/1')
+					.set('content-type', 'application/x-www-form-urlencoded')
+					.send({first_name: firstName})
+					.end((err, res) => {
+						expect(res.body.error).to.equal(false)
+						expect(res.body.data[0].first_name).to.equal(firstName)
+						done()
+					})
+				})
+		})
+		
 })
